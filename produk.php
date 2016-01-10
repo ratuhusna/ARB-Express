5de77d414324c2078f25ac994408168e069bb050
ratuhusna <ratuhusna09@gmail.com>
2016-01-10 10:03:48 +0700

    produk.php

---
+++ produk.php
@@ -0,0 +1,121 @@
+<!DOCTYPE HTML>
+<html lang="en-US" xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
+<head>
+	<title>ARB Express</title>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	
+	<script src="js/jquery-1.6.2.min.js" type="text/javascript" charset="utf-8"></script>
+	<script src="js/jquery.jcarousel.min.js" type="text/javascript" charset="utf-8"></script>
+	<script src="js/cufon-yui.js" type="text/javascript"></script>
+	<script src="js/Verdana_400-Verdana_700.font.js" type="text/javascript"></script>
+	<!--[if IE 6]>
+		<script src="js/DD_belatedPNG-min.js" type="text/javascript" charset="utf-8"></script>
+	<![endif]-->
+	<script src="js/functions.js" type="text/javascript" charset="utf-8"></script>
+</head>
+<body>
+	<!-- Begin Wrapper -->
+	<div id="wrapper">
+		<!-- Begin Shell -->
+		<div class="shell">
+			<div class="cl">&nbsp;</div>
+			<!-- Begin Header -->
+			<div id="header">
+				<h1><a href="home.php" title="ARB Express">ARB Express</a></h1>
+				<!-- Begin Search -->
+				<div id="search">
+					<form action="#" method="get" accept-charset="utf-8">
+						<input type="text" value="Search..." title="Search..." class="blink field" />
+						<input class="search-button" type="submit" value="Submit" />
+						<div class="cl">&nbsp;</div>
+					</form>
+				</div>
+				<!-- End Search -->
+				<div class="cl">&nbsp;</div>
+			</div>
+			<!-- End Header -->
+			<!-- Begin Navigation -->
+			<div id="navigation">
+				<ul>
+					<li><a href="home.php" title="Home"><span>Home</span></a></li>
+					<li><a href="cek.php" title="Cek Resi & Tarif"><span>Cek Resi & Tarif</span></a></li>
+					<li><a href="login.php" title="Login"><span>Login</span></a></li>
+					<li><a href="register.php" title="Register"><span>Register</span></a></li>
+					<li><a href="about.php" title="About"><span>About</span></a></li>
+				</ul>
+				<div class="cl">&nbsp;</div>
+			</div>
+			<!-- End Navigation -->
+			<div class="cl">&nbsp;</div>
+			<!-- Begin Main -->
+			<div id="main">
+				<!-- Begin Content -->
+				<div id="content">
+					<div class="top"></div>
+					<!-- Begin Inner -->
+					<div class="inner">
+						<div id="products">
+							<h2>ARB Reguler</h2>
+							<!-- Begin Row -->
+							<p>Ketentuan Layanan ABR Reguler :</p>
+							<ol>
+								<li>Layanan ARB Regular tidak melayani pengantaran pada hari Minggu dan hari Libur Nasional.</li>
+								<li>Estimasi waktu penyampaian kiriman(estimate delivery time) untuk tujuan yang berada langsung di kota cabang/agen utama adalah 1 hari sampai 3 hari.</li>
+								<li>Layanan ARB Regular dapat dilayani oleh semua cabang/agen utama maupun sub-agen.</li>
+								<li>Layanan ARB Regular tidak memberikan jaminan uang kembali.</li>
+							</ol>
+							<p>Cara penggunaan layanan ini :</p>
+							<ol>
+								<li>Pelanggan dapat memilih layanan ini pada saat pengiriman di setiap counter ARB Express.</li>
+								<li>Kiriman akan disampaikan dalam waktu yang secepatnya, melalui jaringan yang tersedia.</li>
+								<li>Bukti penerimaan dan status kiriman senantiasa tersedia melalui website www.abrexpress.co.id.</li>
+							</ol>
+						</div>
+						<div id="products">
+							<h2>ARB VIP</h2>
+							<!-- Begin Row -->
+							<p>Ketentuan Layanan ARB VIP :</p>
+							<ol>
+								<li>Berlaku untuk kota tujuan yang mempunyai penerbangan langsung.</li>
+								<li>Anda dapat melacak status pengiriman dan informasi penerimaan melalui website ABR Express.</li>
+							</ol>
+							<p>Cara penggunaan layanan ini :</p>
+							<ol>
+								<li>Pelanggan menghubungi contact center, customer service akan mengatur penjemputannya.</li>
+								<li>Sesuai dengan jadwal penerbangan yang tersedia, kiriman akan diberangkatkan.</li>
+								<li>Keberhasilan pengiriman akan disampaikan kepada pelanggan melalui SMS pada kesempatan pertama.</li>
+							</ol>
+						</div>
+						<div id="products">
+							<h2>ARB Box</h2>
+							<!-- Begin Row -->
+							<p>Ketentuan Layanan ARB Box :</p>
+							<ol>
+								<li>Layanan ARB Box tidak melayani pengantaran pada hari Sabtu, Minggu dan hari Libur Nasional.</li>
+								<li>Perkiraan waktu penyampaian kiriman paling cepat 3 hari kerja, tergantung daripada tujuan pengirimannya.</li>
+								<li>Layanan ARB Box tidak ada memberikan jaminan uang kembali.</li>
+							</ol>
+							<p>Cara penggunaan layanan ini :</p>
+							<ol>
+								<li>Pelanggan dapat memilih layanan ini pada saat pengiriman di setiap counter ARB Express.</li>
+								<li>Perkiraan waktu penyampaian kiriman untuk kota besar adalah 3-5 hari.</li>
+							</ol>
+						</div>
+						<!-- End Inner -->
+					</div>
+					<!-- End Content -->
+				</div>
+				<!-- End Main -->
+			<!-- Begin Footer -->
+			<div id="footer">
+				<p>2015 &copy; ARB Express | All Rights Reserved</p>
+				<div class="cl">&nbsp;</div>
+			</div>
+			<!-- End Footer -->
+		</div>
+		<!-- End Shell -->
+	</div>
+	<!-- End Wrapper -->
+</body>
+</html>
