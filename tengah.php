c2fef8686f2c925b9172075fc9c3e5e84e57c535
ratuhusna <ratuhusna09@gmail.com>
2016-01-10 10:11:45 +0700

    tengah.php

---
+++ tengah.php
@@ -0,0 +1,37 @@
+<div class="row">
+  <p>Dengan pengalaman kami, ABR Express melayani kebutuhan pelanggan, dengan jasa pengiriman dalam dan luar negeri.</p>
+  <p>Dan di ARB Express disini juga bisa melihat tarif pengiriman yang murah dengan jenis paket yang berbeda-beda.</p>
+  <h2>Produk</h2>
+  <!-- Begin Row -->
+  <div class="row last">
+    <div class="product">
+      <div class="img-box">
+        <div class="box-frame">&nbsp;</div>
+        <img src="css/images/produk.jpg" alt="Product Image" /> <a href="produk.php" class="more" title="View More">View More</a> </div>
+      <div class="pr-entry">
+        <h4>Reguler</h4>
+        <p>ARB Regular adalah layanan pengiriman cepat, aman, dan handal sampai ke pelosok Indonesia.</p>
+      </div>
+    </div>
+    <div class="product">
+      <div class="img-box">
+        <div class="box-frame">&nbsp;</div>
+        <img src="css/images/produk.jpg" alt="Product Image" /> <a href="produk.php" class="more" title="View More">View More</a> </div>
+      <div class="pr-entry">
+        <h4>VIP</h4>
+        <p>ARB VIP adalah layanan pengiriman premium dengan target kiriman sampai di tujuan pada keesokan harinya.</p>
+      </div>
+    </div>
+    <div class="product pr-last">
+      <div class="img-box">
+        <div class="box-frame">&nbsp;</div>
+        <img src="css/images/produk.jpg" alt="Product Image" /> <a href="produk.php" class="more" title="View More">View More</a> </div>
+      <div class="pr-entry">
+        <h4>Box</h4>
+        <p>ARB Box adalah layanan pengiriman untuk barang berukuran besar atau berat dengan harga ekonomis yang menghubungkan kota-kota besar, ibu kota provinsi, sampai ke kabupaten.</p>
+      </div>
+    </div>
+    <div class="cl">&nbsp;</div>
+  </div>
+  <p>&nbsp;</p>
+</div>
